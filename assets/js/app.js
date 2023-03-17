329f893523508d28c6d3f2af64bc9968da2f2a75
Matheus Alves <54443770+matthaiosalves@users.noreply.github.com>
2023-03-17 18:04:27 -0300

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,3 +2,9 @@ function onClickActive(obj) {
   document.querySelector('#needs').classList.toggle('active');
 }
 document.querySelector('#needs').addEventListener('click', onClickActive);
+
+
+
+document.querySelector(".wrapperForm").addEventListener("click", function(e){ 
+  e.preventDefault();
+});
